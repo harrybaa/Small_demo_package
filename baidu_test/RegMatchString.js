7a2bb6ad515de8b58d9d2863923edb4f2d32528f
Harry Liu <harrybaa@gmail.com>
2017-09-06 15:07:00 +0800

    baidu tests

---
+++ baidu_test/RegMatchString.js
@@ -0,0 +1,54 @@
+// var firstLine = readline();
+// var secondLine = readline();
+
+var firstLine = "abcd";
+var secondLine = "??"
+
+var findMatch = function(firstLine, secondLine) {
+    var count = 0,
+        firstLen = firstLine.length,
+        secondLen = secondLine.length,
+        subStr,
+        uniqueSet = new Set();
+    
+    // 处理边界情况
+    if(firstLen < secondLen || secondLen === 0) {
+        console.log(0);
+        return;
+    }
+    
+    // 一次移一位匹配
+    for(let i = 0; i <= firstLen - secondLen; i++) {
+        subStr = firstLine.substr(i, secondLen);
+        if(isSubMatched(subStr, secondLine)) {
+            // 如果subStr没有在set里
+            if(!uniqueSet.has(subStr)) uniqueSet.add(subStr);
+        }
+    }
+    
+    // 输出结果
+    console.log("Total matched substrings: " + uniqueSet.size);
+    if(uniqueSet.size) {
+        console.log("All matched strings: ");
+        uniqueSet.forEach(item => console.log(item + " "));
+    }
+}
+
+var isSubMatched = function(target, matchString) {
+    // 检测错误实参
+    if(target.length !== matchString.length || target.length === 0) {
+        console.error('Wrong parameter!');
+        return false;
+    }
+        
+    // 匹配
+    for(let i = 0; i < matchString.length; i++) {
+        if(matchString.charAt(i) != target.charAt(i) &&
+           matchString.charAt(i) != '?')
+            return false;
+    }
+    
+    return true;
+}
+
+findMatch(firstLine, secondLine);
